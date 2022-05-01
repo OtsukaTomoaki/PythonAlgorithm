38edb4f50b693c819742a577cfe55890c1f19509
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-05-01 15:34:08 +0900

    バブルソートの実装

---
+++ chapter5/bubble_sort.py
@@ -0,0 +1,9 @@
+data = [6, 15, 4, 2, 8, 5, 11, 9, 7, 13]
+
+for i in range(len(data)):
+    print(data)
+    for j in range(len(data) - i - 1):
+        if data[j] > data[j + 1]:
+            data[j], data[j + 1] = data[j + 1], data[j]
+
+print(data)
