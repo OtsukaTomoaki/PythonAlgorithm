34f0d9c98dc5f08b354a87ed3239f529d7edb9e1
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-05-01 15:38:19 +0900

    ソートずみになった時点で処理を打ち切るようにする

--- chapter5/bubble_sort.py
+++ chapter5/bubble_sort.py
@@ -1,9 +1,14 @@
 data = [6, 15, 4, 2, 8, 5, 11, 9, 7, 13]
 
+change = True
 for i in range(len(data)):
+    if not change:
+        break#バブルソートに変更が生じなかった場合は処理を打ち切る
+    change = False
     print(data)
     for j in range(len(data) - i - 1):
         if data[j] > data[j + 1]:
             data[j], data[j + 1] = data[j + 1], data[j]
+            change = True
 
 print(data)
